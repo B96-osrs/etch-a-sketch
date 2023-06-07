45b9bd391d7ae373253d9efdf3b35ed295b9355a
B96-osrs <115960201+B96-osrs@users.noreply.github.com>
2023-06-07 18:57:31 +0300

    Add logic to drawbox

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 let gridRow = [];
 let gridColumn = [];
 let drawBox = document.getElementById("draw-box");
-
+let canvasTile;
 
 function createGrid(sizeInput) {
     for(let i = 0; i < sizeInput; i++) {
@@ -11,12 +11,47 @@ function createGrid(sizeInput) {
         for(let j = 0; j < sizeInput; j++) {
             gridColumn[j] = document.createElement("div");
             gridRow[i].appendChild(gridColumn[j]);
-            gridColumn[j].setAttribute("id", "columnDiv");
-            gridColumn[j].textContent = `${j}`
+            gridColumn[j].setAttribute("class", "columnDiv");
         }
     }
 
 
 }
 
-createGrid(16);
+createGrid(50);
+canvasTile = document.querySelectorAll(".columnDiv");
+
+canvasTile.forEach(element => {
+    element.addEventListener("mouseover", function(e) {
+        if(e.buttons === 1) {
+        element.style.backgroundColor = "black";
+        }
+    });   
+});
+
+
+
+// for(let i = 0; i < 6; i++) {
+//     const testArray = document.querySelectorAll(".columnDiv");
+//     console.log(testArray[i]);
+// }
+
+
+
+
+
+
+
+// document.querySelectorAll(".columnDiv").forEach(element => {
+//     element.addEventListener("click", function() {
+//         const currentTile = element;
+//         currentTile.style.backgroundColor = "red";
+//     });  
+// });
+
+// scissorsButton.addEventListener("click", function() {
+//     const userPickBox = document.getElementById("user-pick");
+//     userPickBox.textContent ="SCISSORS";
+
+//         playRound("scissors");     
+// });
